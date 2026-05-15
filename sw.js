951334f9eb9c6813a1e54995b4ec327792509514
emeruizruiz <emeruizruiz@gmail.com>
2026-05-15 14:07:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = "gymrat-v1";
 const ASSETS = [
-  "/index.html",
-  "/manifest.json"
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
@@ -20,6 +20,6 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("/index.html")))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("./index.html")))
   );
 });
